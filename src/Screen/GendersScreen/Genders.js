4947449672dec8bfd97d0ea17d377dd840588c10
Talha Mert Karakoç <landienzla@gmail.com>
2022-01-26 17:51:23 +0300

    genders screen -i probably won't add this screen to main app-

--- src/Screen/GendersScreen/Genders.js
+++ src/Screen/GendersScreen/Genders.js
@@ -1,9 +1,14 @@
-import {View, Text,SafeAreaView,ImageBackground} from 'react-native';
-import CheckBox from '@react-native-community/checkbox';
-import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import Male from '../../images/male.png'
-import Female from '../../images/female.png'
+import Male from '../../images/male.png';
+import Female from '../../images/female.png';
 const StyledBackground = styled.SafeAreaView`
   background-color: #f8f8f8;
   flex: 1;
@@ -37,29 +42,90 @@ const Line = styled.View`
   left: 250px;
 `;
 const StyledGenderText = styled.Text`
-position: absolute;
-font-family: Faustina-Regular;
-font-size: 36px;
-color: #000000;
-top: 180px;
-`
+  position: absolute;
+  font-family: Faustina-Regular;
+  font-size: 36px;
+  color: #000000;
+  top: 120px;
+`;
+const StyledGenderView = styled.TouchableOpacity``;
 const StyledGenderBox = styled.ImageBackground`
-width: 150px;
-height: 150px;
-border: 1px solid grey;
-border-radius: 10px;
-`
+  width: 150px;
+  height: 150px;
+  border: 1px solid grey;
+  border-radius: 10px;
+`;
+const StyledButton = styled.TouchableOpacity`
+  background-color: #c12323;
+  width: 390px;
+  height: 60px;
+  position: absolute;
+  top: 520px;
+  border-radius: 5px;
+  align-items: center;
+`;
+const StyledNextText = styled.Text`
+  color: #f8f8f8;
+  font-family: Faustina-SemiBold;
+  font-size: 24px;
+  top: 14px;
+  letter-spacing: 2px;
+`;
+const StyledBetterExpText = styled.Text`
+  font-family: Faustina-Regular;
+  color: #777777;
+  top: 600px;
+  position: absolute;
+  font-size: 18px;
+  width: 245px;
+  text-align: center;
+`;
 export default function Genders() {
+  const [selectedGender, setSelectedGender] = useState();
+  const genderHandler = e => {
+    const {nativeID} = e._dispatchInstances.pendingProps;
+    setSelectedGender(nativeID);
+  };
   return (
     <StyledBackground>
       <StyledLandiFitText>
         <Landi>Landi</Landi>
         <Fit>Fit</Fit>
       </StyledLandiFitText>
-      <Line/>
-      <StyledGenderText>Gender</StyledGenderText>  
-      <StyledGenderBox source={Male} style={{top: 250}}><CheckBox></CheckBox></StyledGenderBox>
-      <StyledGenderBox source={Female} style={{top: 270}}/>
+      <Line />
+      <StyledGenderText>Gender</StyledGenderText>
+      <StyledGenderView
+      style={{top: 190}}
+        onPress={e => {
+          genderHandler(e);
+        }}
+        nativeID="Male">
+        <StyledGenderBox
+          source={Male}
+          style={{
+            borderColor: selectedGender === 'Male' ? 'green' : 'grey',
+          }}></StyledGenderBox>
+      </StyledGenderView>
+      <StyledGenderView
+      style={{top: 210}}
+        onPress={e => {
+          genderHandler(e);
+        }}
+        key="Female"
+        nativeID="Female">
+        <StyledGenderBox
+          source={Female}
+          style={{
+            borderColor: selectedGender === 'Female' ? 'green' : 'grey',
+          }}
+        />
+      </StyledGenderView>
+      <StyledButton>
+        <StyledNextText>Next</StyledNextText>
+      </StyledButton>
+      <StyledBetterExpText>
+        To give you a better experrince we need to know your Gender{' '}
+      </StyledBetterExpText>
     </StyledBackground>
   );
 }
