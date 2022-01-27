d8a57d355bfcdfd664087fae69b25e5ab79fe2b3
Talha Mert Karakoç <landienzla@gmail.com>
2022-01-27 15:41:14 +0300

    Home Screen => completed 75%

--- App.js
+++ App.js
@@ -7,22 +7,83 @@ import SignIn from './src/Screen/SignInScreen/SignIn';
 import SignUp from './src/Screen/SignUpScreen/SignUp';
 import Genders from './src/Screen/GendersScreen/Genders';
 import Focus from './src/Screen/FocusScreen/Focus';
+import Home from './src/Screen/HomeScreen/Home';
+import {Button, Text, View,ImageBackground,Image} from 'react-native';
+import sideBarImage from './src/images/sideBar.png'
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Splash1'
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Splash1" component={Splash1} />
-        <Stack.Screen name="Sign" component={Sign} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Genders" component={Genders} />
-        <Stack.Screen name="Focus" component={Focus}/>
+      <Stack.Navigator
+        initialRouteName="Home"
+        // screenOptions={{
+        //   headerShown: true,
+        // }}
+      >
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="Splash1"
+          component={Splash1}
+        />
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="Sign"
+          component={Sign}
+        />
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="SignIn"
+          component={SignIn}
+        />
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="SignUp"
+          component={SignUp}
+        />
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="Genders"
+          component={Genders}
+        />
+        <Stack.Screen
+          options={headerOptions.preventHeader}
+          name="Focus"
+          component={Focus}
+        />
+        <Stack.Screen
+          options={{
+            headerTitle: () => (
+              <View style={{flex: 1}}>
+                <Text
+                  style={{
+                    fontFamily: 'Faustina-Regular',
+                    fontSize: 24,
+                    color: 'white',
+                    height: 30,
+                    textAlign: 'center',
+                  }}>
+                  Home
+                </Text>
+              </View>
+            ),
+            headerStyle: {
+              backgroundColor: '#c12323',
+              alignItems: "center"
+            },
+          }}
+          name="Home"
+          component={Home}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+const headerOptions = {
+  allowHeader: {
+    headerShown: true,
+  },
+  preventHeader: {
+    headerShown: false,
+  },
+};
---
+++ src/Screen/HomeScreen/Home.js
@@ -0,0 +1,208 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  ImageBackground,
+} from 'react-native';
+import React from 'react';
+import styled from 'styled-components';
+const StyledBackground = styled.SafeAreaView`
+  background-color: #f8f8f8;
+  flex: 1;
+`;
+const StyledWorkoutTitle = styled.Text`
+  font-size: 28px;
+  font-family: Faustina-Regular;
+  color: black;
+  position: absolute;
+  left: 20px;
+  margin-top: 10px;
+`;
+const StyledWorkoutFlatList = styled.FlatList`
+  top: 60px;
+  position: absolute;
+`;
+const StyledWorkoutView = styled.View`
+  width: 130px;
+  height: 160px;
+  margin: 4px;
+  background-color: grey;
+  border-radius: 5px;
+  flex: 1;
+`;
+const StyledWorkOutItemTitle = styled.Text`
+  color: white;
+  font-size: 22px;
+  top: 100px;
+  width: 113px;
+  font-family: Faustina-Regular;
+  flex: 1;
+`;
+const StyledTrainingTitle = styled.Text`
+  font-size: 28px;
+  font-family: Faustina-Regular;
+  color: black;
+  position: absolute;
+  left: 20px;
+  margin-top: 10px;
+  top: 230px;
+`;
+const StyledTrainingFlatList = styled.FlatList`
+  top: 290px;
+  position: relative;
+`;
+const StyledTrainingView = styled.View`
+  width: 182px;
+  height: 200px;
+  background-color: #cecece;
+  margin: 10px;
+  border-radius: 5px;
+`;
+const StyledTrainingImage = styled.Image`
+  width: 50px;
+  height: 50px;
+  margin: 10px;
+`;
+const StyledTrainingItemTitle = styled.Text`
+  font-family: Faustina-Medium;
+  color: black;
+  font-size: 18px;
+  margin-left: 10px;
+`;
+const StyledTrainingItemDescription = styled.Text`
+  color: #777777;
+  margin-left: 10px;
+  margin-top: 10px;
+  font-size: 15px;
+`;
+export default function Home() {
+  const workoutMockData = [
+    {
+      id: 1,
+      title: 'Abs Perfection',
+      image: require('../../images/workoutMockImage.png'),
+    },
+    {
+      id: 2,
+      title: 'Good Cardio',
+      image: require('../../images/workoutMockImage.png'),
+    },
+    {
+      id: 3,
+      title: 'Arms  Stretching',
+      image: require('../../images/workoutMockImage.png'),
+    },
+    {
+      id: 4,
+      title: 'Arms  Stretching',
+      image: require('../../images/workoutMockImage.png'),
+    },
+    {
+      id: 5,
+      title: 'Arms  Stretching',
+      image: require('../../images/workoutMockImage.png'),
+    },
+    {
+      id: 6,
+      title: 'Arms  Stretching',
+      image: require('../../images/workoutMockImage.png'),
+    },
+  ];
+  const trainingMockData = [
+    {
+      id: 1,
+      title: 'Shoulder Press',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 2,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 3,
+      title: 'Shoulder Press',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 4,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 5,
+      title: 'Shoulder Press',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 6,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 7,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 8,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 9,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+    {
+      id: 10,
+      title: 'Jogging',
+      description: 'Walking is simple, yet powerful exercise for your body',
+      image: require('../../images/trainingMockImage.png'),
+    },
+  ];
+  return (
+    <StyledBackground>
+      <StyledWorkoutTitle>Workout Exercises</StyledWorkoutTitle>
+      <StyledWorkoutFlatList
+        horizontal
+        data={workoutMockData}
+        renderItem={({item}) => (
+          <StyledWorkoutView>
+            <ImageBackground
+              source={item.image}
+              resizeMode="cover"
+              style={{flex: 1}}>
+              <StyledWorkOutItemTitle>{item.title}</StyledWorkOutItemTitle>
+            </ImageBackground>
+          </StyledWorkoutView>
+        )}
+      />
+      <StyledTrainingTitle>Training Plan</StyledTrainingTitle>
+      <StyledTrainingFlatList
+        data={trainingMockData}
+        renderItem={({item}) => (
+          <StyledTrainingView>
+            <StyledTrainingImage source={item.image} />
+            <StyledTrainingItemTitle>{item.title}</StyledTrainingItemTitle>
+            <StyledTrainingItemDescription>
+              {item.description}
+            </StyledTrainingItemDescription>
+          </StyledTrainingView>
+        )}
+        numColumns={2}
+        keyExtractor={item => item.id} 
+      />
+    </StyledBackground>
+  );
+}
